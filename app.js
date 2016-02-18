1ec3c36d5be4e9cd28d805bfd7421585c25653ec
Taco <travis_johnson@volusion.com>
2016-02-17 21:00:08 -0600

    Switched app to default to static JS

--- app.js
+++ app.js
@@ -8,7 +8,15 @@ app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 
 app.get('/', function(req, res) {
-    res.render('index', { title: 'Syn UI' });
+    res.render('index', {
+        title: 'Syn UI',
+        //scripts: 'http://localhost:8080/lib/bundle.js'
+        scripts: '/bundle.js'
+    });
+});
+
+app.get('/bundle.js', function (req, res) {
+    res.sendFile(path.join(__dirname, 'lib/bundle.js'));
 });
 
 app.listen(3000, function() {
