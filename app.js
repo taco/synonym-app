afb7dfad2f8abb181fceb940dee744d6bcb9bc8f
Taco <travis_johnson@volusion.com>
2016-02-17 21:08:48 -0600

    Added .env support for development (hot reloading) and production (static bundle.js)

--- app.js
+++ app.js
@@ -1,4 +1,6 @@
 /* eslint-disable */
+require('dotenv').config({ silent: true });
+
 const express = require('express');
 const path = require('path');
 
@@ -8,10 +10,14 @@ app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 
 app.get('/', function(req, res) {
+
+    const scripts = process.env.NODE_ENV === 'production'
+                        ? '/bundle.js'
+                        : 'http://localhost:8080/lib/bundle.js';
+
     res.render('index', {
         title: 'Syn UI',
-        //scripts: 'http://localhost:8080/lib/bundle.js'
-        scripts: '/bundle.js'
+        scripts: scripts
     });
 });
 
